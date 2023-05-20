913d835432f6ba491662a2690006d3fe70178b6f
Akshat Jaimini <destrex271@gmail.com>
2023-05-20 17:16:09 +0530

    repository setup for tests

--- src/workflow_utils/setup.sh
+++ src/workflow_utils/setup.sh
@@ -2,4 +2,5 @@ git clone https://github.com/destrex271/pgweb-testing-harness
 git clone git://git.postgresql.org/git/pgweb.git
 cd pgweb
 pip install -r requirements.txt # Setting up the postgresql website requirements
-python manage.py shell <../pgweb_testing_harness/src/functional_tests/tests.py
+ls
+# python manage.py test
