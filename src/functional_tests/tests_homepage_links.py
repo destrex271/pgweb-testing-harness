c3631c82858662fd92ff8eb251aabb31f09f21f0
Akshat Jaimini <destrex271@gmail.com>
2023-05-30 13:14:42 +0530

    link test added back

--- src/functional_tests/tests_homepage_links.py
+++ src/functional_tests/tests_homepage_links.py
@@ -1,12 +1,7 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
-# from webdriver_manager.chrome import ChromeType
 from webdriver_manager.firefox import GeckoDriverManager
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
 from selenium import webdriver
-from selenium.webdriver.firefox.webdriver import WebDriver
 
 class MySeleniumTests(StaticLiveServerTestCase):
 
