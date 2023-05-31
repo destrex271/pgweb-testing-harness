72a61a6077c060d0c849fb1ea07e2044804462ef
Akshat Jaimini <destrex271@gmail.com>
2023-05-31 15:25:12 +0530

    Tests for website level links(internal links) and external links added

--- src/functional_tests/tests_homepage_links.py
+++ src/functional_tests/tests_homepage_links.py
@@ -3,7 +3,7 @@ from django.test.testcases import call_command, connections
 from selenium.webdriver.common.by import By
 from webdriver_manager.firefox import GeckoDriverManager
 from selenium import webdriver
-import urllib
+import requests
 
 def _fixture_teardown(self):
     # Allow TRUNCATE ... CASCADE and don't emit the post_migrate signal
@@ -29,23 +29,65 @@ def _fixture_teardown(self):
             inhibit_post_migrate=inhibit_post_migrate,
         )
 
-print("OKK")
 LiveServerTestCase._fixture_teardown = _fixture_teardown
+
+external_links = []
+internal_links = []
+broken_internal_links = []
+broken_external_links = []
+
+
+def segregate_links(seln, addr):
+    seln.get(f"{addr}/")
+    links = seln.find_elements(By.TAG_NAME, 'a')
+    for link in links:
+        lnk = link.get_attribute('href')
+        if lnk.__contains__('localhost'):
+            internal_links.append(lnk)
+        else:
+            external_links.append(lnk)
+
 
 class RecusrsiveLinkCrawlTests(LiveServerTestCase):
 
-    # def setUpClass(cls):
-    #     super().setUpClass()
-    #     options = webdriver.FirefoxOptions()
-    #     options.headless = True
-    #     cls.selenium = webdriver.Firefox(executable_path=GeckoDriverManager().install(), options=options)
-    #     cls.selenium.implicitly_wait(10)
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        options = webdriver.FirefoxOptions()
+        options.headless = True
+        cls.selenium = webdriver.Firefox(executable_path=GeckoDriverManager().install(), options=options)
+        segregate_links(cls.selenium, cls.live_server_url)
 
-    # @classmethod
-    # def tearDownClass(cls):
-    #     cls.selenium.quit()
-    #     super().tearDownClass()
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super().tearDownClass()
 
-    def test_home_page_links(self):
-        print(self.live_server_url)
-        self.assertTrue(True)
+    def test_external_links(self):
+        for lnk in external_links:
+            print(lnk)
+            if not requests.get(lnk).status_code == 200:
+                broken_external_links.append(lnk)
+        self.assertLessEqual(len(broken_external_links), 1, msg=broken_external_links)
+
+    def test_internal_links(self):
+        for lnk in internal_links:
+            print(lnk)
+            if not requests.get(lnk).status_code == 200:
+                broken_internal_links.append(lnk)
+        self.assertLessEqual(len(broken_internal_links), 1, msg=broken_internal_links)
+
+
+# class RecusrsiveLinkCrawlTests_InternalLinks(LiveServerTestCase):
+#     
+#     @classmethod
+#     def setUpClass(cls):
+#         super().setUpClass()
+#         options = webdriver.FirefoxOptions()
+#         options.headless = True
+#         cls.selenium = webdriver.Firefox(executable_path=GeckoDriverManager().install(), options=options)
+#
+#     @classmethod
+#     def tearDownClass(cls):
+#         cls.selenium.quit()
+#         super().tearDownClass()
