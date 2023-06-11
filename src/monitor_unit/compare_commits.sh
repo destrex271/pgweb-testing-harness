e930311f10139404f94e1384a9185dfecd319b63
Akshat Jaimini <destrex271@gmail.com>
2023-06-11 16:40:42 +0530

    Feature: new approach to poll main repository for changes

---
+++ src/monitor_unit/compare_commits.sh
@@ -0,0 +1,29 @@
+function trigger() {
+	REPO_OWNER="destrex271"
+	REPO_NAME="pgweb-testing-harness"
+	WORKFLOW_NAME="run_functional_tests.yml"
+	GITHUB_TOKEN="$SECRET_KEY"
+
+	# Trigger the workflow using the GitHub REST API with authentication
+	curl -X POST \
+		-H "Accept: application/vnd.github.v3+json" \
+		-H "Authorization: token $GITHUB_TOKEN" \
+		"https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/actions/workflows/$WORKFLOW_NAME/dispatches" \
+		-d '{"ref":"main"}'
+}
+
+git clone git://git.postgresql.org/git/pgweb.git
+
+ls
+strp=$(cat commit_id.txt)
+cd pgweb
+echo "$strp"
+echo "$(git rev-parse HEAD)"
+if [ "$strp" != "$(git rev-parse HEAD)" ]; then
+	echo "OKKKK"
+	echo "Triggering"
+	trigger
+fi
+
+cd ..
+rm -rf pgweb
---
+++ src/monitor_unit/update_commit_value.sh
@@ -0,0 +1,17 @@
+git clone git://git.postgresql.org/git/pgweb.git
+cd pgweb
+
+# Get last commit id
+git rev-parse HEAD >"commit_id.txt"
+cat commit_id.txt
+cd ..
+rm -rf pgweb
+
+# Push to repository
+git add .
+git config --global user.name "destrex271"
+git config --global user.email "destrex271@gmail.com"
+
+git add -A
+git commit -m "GH Action; Updated commit id"
+git push
