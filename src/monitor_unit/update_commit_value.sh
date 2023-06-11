3c545b03b637ad717e84cc2d38db65ad36951576
Akshat Jaimini <destrex271@gmail.com>
2023-06-11 17:04:35 +0530

    repo owner var added

--- src/monitor_unit/update_commit_value.sh
+++ src/monitor_unit/update_commit_value.sh
@@ -1,4 +1,5 @@
 REPO_NAME="pgweb-testing-harness"
+REPO_OWNER="destrex271"
 GITHUB_TOKEN="$SECRET_KEY"
 
 git clone git://git.postgresql.org/git/pgweb.git
@@ -17,5 +18,5 @@ git config --global user.email "destrex271@gmail.com"
 
 git add .
 git commit -m "GH Action; Updated commit id"
-git remote set-url origin https://x-access-token:$SECRET_KEY@github.com/$REPO_NAME
+git remote set-url origin https://x-access-token:$SECRET_KEY@github.com/$REPO_OWNER/$REPO_NAME
 git push -u origin main
