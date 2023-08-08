ead2b0a2598aba3aac7bfdebfa347990270e9f92
destrex271 <destrex271@gmail.com>
2023-08-08 12:44:46 +0530

    Added threadpool to execute simultaneous lighthouse tests

--- src/accessibility_tests/tests_w3c_rules.py
+++ src/accessibility_tests/tests_w3c_rules.py
@@ -1,6 +1,9 @@
 from django.contrib.staticfiles.testing import LiveServerTestCase
 from django.test.testcases import call_command, connections
 import requests
+import json
+import concurrent.futures
+from concurrent.futures import ThreadPoolExecutor
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
@@ -71,6 +74,20 @@ def prepare_site_map(base):
         del urls[0]
 
 
+def run_lighthouse(url_lst):
+    main_data = {}
+    for url in url_lst:
+            os.system(f'lighthouse --chrome-flags="--no-sandbox --headless --disable-gpu" --only-categories accessibility --disable-storage-reset="true" --output=json --output-path ./report.json {url}')
+            with open('./report.json', 'r') as f:
+                js_data = json.loads(f.read())
+                print(js_data)
+                for x in js_data.keys():
+                    print(x)
+                # print(js_data['categories'])
+                main_data[url] = js_data['categories']
+    return main_data
+
+
 class AccessibilityTests(LiveServerTestCase):
 
     fixtures = ['pgweb/fixtures/users.json', 'pgweb/fixtures/org_types.json',
@@ -96,12 +113,35 @@ class AccessibilityTests(LiveServerTestCase):
         cls.selenium.quit()
         super().tearDownClass()
 
-    def tests_accessibility_issues(self):
+    def tests_accessibility_issues(self)::
         prepare_site_map(self.live_server_url)
-        os.system(f'''lighthouse {self.live_server_url} --only-categories accessibility --chrome-flags="--no-sandbox --headless --disable-gpu" --output json --output-path ./report.json''')
-        # os.system('''pa11y-ci --sitemap https://pa11y.org/sitemap.xml -c ./pgweb/conf''')
-        # os.system(f'''lightcrawler --url {self.live_server_url} --config ./pgweb/conf.json --chrome-flags="--no-sandbox --headless --disable-gpu --output=html --output-path ./a.html"''')
-        # os.system("ls")
-        print("----------------------------------")
-        # os.system("ls pgweb")
-        # os.system("cat ./a.json")
+        size = len(site_map)//20
+        threads = ThreadPoolExecutor(size)
+        i = 1
+        lst = []
+        tasks = []
+        for url in site_map:
+            lst.append(url)
+            if i % 20 == 0:
+                tasks.append(threads.submit(run_lighthouse, lst))
+                lst = []
+                lst.append(url)
+            i += 1
+
+        main_data = {}
+        ftasks = concurrent.futures.as_completed(tasks)
+        for ftask in ftasks:
+            try:
+                data = ftask.result()
+                print(data)
+                main_data.update(data)
+            except Exception as ex:
+                print(ex)
+                self.assertTrue(False, msg='Error in rendering documentation')
+            else:
+                print(data)
+
+        print(main_data)
+        if main_data:
+            with open('output.json', 'w') as f:
+                json.dump(main_data, f)
