64d721c9686f0f8b5c832bb80217253b8d0b4b45
destrex271 <destrex271@gmail.com>
2023-08-09 23:04:13 +0530

    fix typos

--- src/workflow_utils/send_mail.py
+++ src/workflow_utils/send_mail.py
@@ -2,6 +2,7 @@ import smtplib, ssl
 import os
 
 port = 465
+# port = 587
 smtp_server = "smtp.gmail.com"
 USERNAME = os.environ.get('USERNAME')
 PASSWORD = os.environ.get('PASSWORD')
@@ -14,7 +15,7 @@ Please visit the actions page for more info on the latest run.
 
 
 context = ssl.create_default_context()
-with smtplib.SMTP_SSL(smtp_server, port, context) as server:
+with smtplib.SMTP_SSL(smtp_server, port) as server:
     server.login(USERNAME, PASSWORD)
     server.sendmail(USERNAME, 'akshatdev2711@gmail.com', message)
 
