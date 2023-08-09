2d02f67a291b07e317807bb462fd10ca62a5fd05
destrex271 <destrex271@gmail.com>
2023-08-10 00:01:39 +0530

    Added loop function

--- src/workflow_utils/send_mail.py
+++ src/workflow_utils/send_mail.py
@@ -4,7 +4,6 @@ import os
 from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from email.utils import COMMASPACE, formatdate
 import json
 
 port = 465
@@ -20,25 +19,28 @@ print(us_dict)
 USERNAME = os.environ.get('USERNAME')
 PASSWORD = os.environ.get('PASSWORD')
 files = ['./src/failed_tests.log', './src/final_report.log']
-# files = ['commit_id.txt']
 message = '''
 Please visit the actions page for more info on the latest run.
 '''
+for to_user in us_dict['to']:
+    msg = MIMEMultipart()
+    msg['From'] = USERNAME
+    msg['To'] = to_user
+    msg['Subject'] = 'Testing Harness Reports for latest commit'
+    msg.attach(MIMEText(message, 'plain'))
 
-msg = MIMEMultipart()
-msg['From'] = USERNAME
-msg['To'] = us_dict['to']
-msg['Date'] = formatdate(localtime=True)
-msg['Subject'] = 'Testing Harness Reports for latest commit'
-msg.attach(MIMEText(message))
+    for file in files:
+        print(file)
+        with open(file, "rb") as f:
+            part = MIMEApplication(f.read(), _subtype="log")
+            part.add_header('content-disposition', f'attachment; filename={file}')
+            msg.attach(part)
 
-for file in files:
-    with open(file, "rb") as f:
-        part = MIMEApplication(f.read(), _subtype="log")
-        part.add_header('content-disposition', f'attachment; filename={file}')
-        msg.attach(part)
-
-context = ssl.create_default_context()
-with smtplib.SMTP_SSL(smtp_server, port) as server:
-    server.login(USERNAME, PASSWORD)
-    server.sendmail(USERNAME, 'akshatdev2711@gmail.com', msg)
+    print("sending msg")
+    a = msg.as_string()
+    with smtplib.SMTP_SSL(smtp_server, port) as server:
+        # server.login(USERNAME, PASSWORD)
+        server.login(USERNAME, PASSWORD)
+        print("Logged in")
+        server.sendmail(USERNAME, to_user, msg.as_string())
+        print("SENT")
