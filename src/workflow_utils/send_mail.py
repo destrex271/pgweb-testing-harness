f83cd5a75f6f8042a2a68db4d0752e4428b0ac40
destrex271 <destrex271@gmail.com>
2023-08-09 23:46:11 +0530

    Add attachments

--- src/workflow_utils/send_mail.py
+++ src/workflow_utils/send_mail.py
@@ -1,21 +1,44 @@
+from os.path import basename
 import smtplib, ssl
 import os
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.utils import COMMASPACE, formatdate
+import json
 
 port = 465
-# port = 587
 smtp_server = "smtp.gmail.com"
+
+us_dict = {}
+
+with open('./src/workflow_utils/recp.json', 'r') as f:
+    us_dict = json.load(f)
+
+print(us_dict)
+
 USERNAME = os.environ.get('USERNAME')
 PASSWORD = os.environ.get('PASSWORD')
-
-
+files = ['./src/failed_tests.log', './src/final_report.log']
+# files = ['commit_id.txt']
 message = '''
-Subject : Pgweb test report
 Please visit the actions page for more info on the latest run.
 '''
 
+msg = MIMEMultipart()
+msg['From'] = USERNAME
+msg['To'] = us_dict['to']
+msg['Date'] = formatdate(localtime=True)
+msg['Subject'] = 'Testing Harness Reports for latest commit'
+msg.attach(MIMEText(message))
+
+for file in files:
+    with open(file, "rb") as f:
+        part = MIMEApplication(f.read(), _subtype="log")
+        part.add_header('content-disposition', f'attachment; filename={file}')
+        msg.attach(part)
 
 context = ssl.create_default_context()
 with smtplib.SMTP_SSL(smtp_server, port) as server:
     server.login(USERNAME, PASSWORD)
-    server.sendmail(USERNAME, 'akshatdev2711@gmail.com', message)
-
+    server.sendmail(USERNAME, 'akshatdev2711@gmail.com', msg)
