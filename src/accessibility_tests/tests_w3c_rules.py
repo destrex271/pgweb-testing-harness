21b722b058f03218eb9a8468aba70691d7e8656f
destrex271 <destrex271@gmail.com>
2023-08-16 12:04:56 +0530

    sitemap translation

--- src/accessibility_tests/tests_w3c_rules.py
+++ src/accessibility_tests/tests_w3c_rules.py
@@ -51,11 +51,11 @@ site_map = []
 
 def translate_sitemap(base):
     global site_map
-    if base.endswith('/'):
-        base = base[0:-1]
-    base += "/sitemap.xml"
-    print(base)
-    # base = "https://www.postgresql.org/sitemap.xml"
+    # if base.endswith('/'):
+    #     base = base[0:-1]
+    # base += "/sitemap.xml"
+    # print(base)
+    base = "https://www.postgresql.org/sitemap.xml"
     res = requests.get(base)
     print(res)
     if res.status_code == 200:
@@ -64,7 +64,6 @@ def translate_sitemap(base):
         for loc in root.findall('.//{http://www.sitemaps.org/schemas/sitemap/0.9}loc'):
             urls.append(loc.text)
         site_map = urls
-    print(site_map)
 
 
 def prepare_site_map(base):
@@ -142,38 +141,38 @@ class AccessibilityTests(LiveServerTestCase):
         # prepare_site_map(self.live_server_url)
         translate_sitemap(self.live_server_url)
         self.assertLess(len(site_map), 0)
-        # threads = ThreadPoolExecutor(len(site_map)//10)
-        # print(len(site_map))
-        # self.assertTrue(len(site_map) > 0, msg="Unable to Translate Sitemap; Please check the website sitemap")
-        # i = 1
-        # lst = []
-        # tasks = []
-        # # main_data = run_lighthouse(site_map)
-        # for url in site_map:
-        #     print(i)
-        #     lst.append(url)
-        #     if i == 10:
-        #         break
-        #     if i % 10 == 0:
-        #         tasks.append(threads.submit(run_lighthouse, lst))
-        #         lst = []
-        #         print(lst)
-        #         lst.append(url)
-        #     i += 1
-        # #
-        # main_data = {}
-        # ftasks = concurrent.futures.as_completed(tasks)
-        # for ftask in ftasks:
-        #     try:
-        #         data = ftask.result()
-        #         print(data)
-        #         main_data.update(data)
-        #     except Exception as ex:
-        #         print(ex)
-        #         self.assertTrue(False, msg='Error in rendering documentation')
-        #     else:
-        #         print(data)
-        # print(main_data)
-        # if main_data:
-        #     with open('output.json', 'w') as f:
-        #         json.dump(main_data, f)
+        threads = ThreadPoolExecutor(len(site_map)//10)
+        print(len(site_map))
+        self.assertTrue(len(site_map) > 0, msg="Unable to Translate Sitemap; Please check the website sitemap")
+        i = 1
+        lst = []
+        tasks = []
+        # main_data = run_lighthouse(site_map)
+        for url in site_map:
+            print(i)
+            lst.append(url)
+            if i == 10:
+                break
+            if i % 10 == 0:
+                tasks.append(threads.submit(run_lighthouse, lst))
+                lst = []
+                print(lst)
+                lst.append(url)
+            i += 1
+        #
+        main_data = {}
+        ftasks = concurrent.futures.as_completed(tasks)
+        for ftask in ftasks:
+            try:
+                data = ftask.result()
+                print(data)
+                main_data.update(data)
+            except Exception as ex:
+                print(ex)
+                self.assertTrue(False, msg='Error in rendering documentation')
+            else:
+                print(data)
+        print(main_data)
+        if main_data:
+            with open('output.json', 'w') as f:
+                json.dump(main_data, f)
