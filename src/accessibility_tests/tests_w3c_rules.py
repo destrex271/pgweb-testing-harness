10987b3a6f8b2fc1150ffb62ff39d19df1421c2f
destrex271 <destrex271@gmail.com>
2023-08-19 15:24:52 +0530

    sitemap translation

--- src/accessibility_tests/tests_w3c_rules.py
+++ src/accessibility_tests/tests_w3c_rules.py
@@ -55,6 +55,7 @@ def translate_sitemap(base):
     #     base = base[0:-1]
     # base += "/sitemap.xml"
     # print(base)
+    base_adr = base
     base = "https://www.postgresql.org/sitemap.xml"
     res = requests.get(base)
     print(res)
@@ -62,7 +63,7 @@ def translate_sitemap(base):
         root = ET.fromstring(res.content)
         urls = []
         for loc in root.findall('.//{http://www.sitemaps.org/schemas/sitemap/0.9}loc'):
-            urls.append(loc.text)
+            urls.append(str(loc.text).replace("www.postgresql.org", base_adr))
         site_map = urls
 
 
@@ -99,10 +100,10 @@ def translate_sitemap(base):
 #
 
 def run_lighthouse(url_lst):
-    print("Testing: ", url_lst)
+    print("Testing: ", {url_lst})
     main_data = {}
     for url in url_lst:
-            os.system(f'lighthouse --chrome-flags="--no-sandbox --headless --disable-gpu" --only-categories accessibility --disable-storage-reset="true" --output=html {url}')
+            os.system(f'lighthouse --chrome-flags="--no-sandbox --headless --disable-gpu" --only-categories accessibility --disable-storage-reset="true" {url}')
             with open('./report.json', 'r') as f:
                 js_data = json.loads(f.read())
                 print(js_data)
@@ -139,17 +140,18 @@ class AccessibilityTests(LiveServerTestCase):
         super().tearDownClass()
 
     def tests_accessibility_issues(self):
+        # prepare_site_map(self.live_server_url)
         translate_sitemap(self.live_server_url)
+        # self.assertLess(len(site_map), 0)
         threads = ThreadPoolExecutor(len(site_map)//10)
         print(len(site_map))
         self.assertTrue(len(site_map) > 0, msg="Unable to Translate Sitemap; Please check the website sitemap")
         i = 1
         lst = []
         tasks = []
+        # main_data = run_lighthouse(site_map)
         for url in site_map:
             print(i)
-            if i == 11:
-                break
             if i % 10 != 0:
                 lst.append(url)
             else:
@@ -158,8 +160,6 @@ class AccessibilityTests(LiveServerTestCase):
                 print(lst)
                 lst.append(url)
             i += 1
-        #
-        main_data = {}
         ftasks = concurrent.futures.as_completed(tasks)
         for ftask in ftasks:
             print(ftask)
@@ -172,8 +172,9 @@ class AccessibilityTests(LiveServerTestCase):
                 self.assertTrue(False, msg='Error in rendering documentation')
             else:
                 print(data)
+        print(os.system('ls ../'))
         print(os.system('ls'))
         # print(main_data)
-        if main_data:
-            with open('output.json', 'w') as f:
-                json.dump(main_data, f)
+        # if main_data:
+        #     with open('output.json', 'w') as f:
+        #         json.dump(main_data, f)
