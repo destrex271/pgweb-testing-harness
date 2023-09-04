0371abcf472a1f77bf04385e25cbf0e12126ff6e
Akshat Jaimini <destrex271@gmail.com>
2023-09-05 00:45:10 +0530

    Update setup.sh

--- src/workflow_utils/setup.sh
+++ src/workflow_utils/setup.sh
@@ -66,7 +66,7 @@ PGPASSWORD=postgres psql -h localhost -U postgres -a -f sql/varnish_local.sql
 # yes | ./pgweb/load_initial_data.sh
 # ./manage.py test --pattern="tests_*.py" --keepdb --verbosity=2 2>&1 | tee -a ../../final_report.log
 # ./manage.py test --pattern="tests_re*.py" --keepdb --verbosity=2 2>&1 | tee -a ../../final_report.log
-./manage.py test --pattern="tests_acc*.py" --keepdb --verbosity=2 2>&1 | tee -a ../../final_report.log
+./manage.py test --pattern="tests_*.py" --keepdb --verbosity=2 2>&1 | tee -a ../../final_report.log
 
 python ../../utils/process_logs.py
 cat ../../final_report.log
