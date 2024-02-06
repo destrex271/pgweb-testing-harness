9bde2bf61bdba8355cf33cfab79591bbf73f78da
Faakhir30 <zahidfaakhir@gmail.com>
2024-02-07 00:04:00 +0500

    Enhancement: Custom Crawler with sub-page depth

---
+++ src/functional_tests/extra_utils/crawler.py
@@ -0,0 +1,46 @@
+from collections import deque
+import requests
+from bs4 import BeautifulSoup as BSoup
+
+class CustomCrawler:
+    def __init__(self, base_url):
+        self.base_url = base_url
+        self.external_links = []
+        self.internal_links = []
+        self.parent_url_dict = {}
+
+    def _extract_links(self, url):
+        page = requests.get(url).content
+        content = BSoup(page, "html.parser")
+        links = content.find_all('a')
+        for lk in links:
+            href = lk.get('href')
+            if href and href != '':
+                if href.startswith('/'):
+                    href = self.base_url + href
+                if href.endswith('.html') and href.startswith('ftp') and not (href.startswith('http') or
+                                                                              href.startswith('/')) or href.__contains__('.html#'):
+                    continue
+                if href.startswith('http') and not (href.startswith('mailto') or href.startswith('#')):
+                    if href.__contains__("localhost") or href.startswith('/'):
+                        self.internal_links.append(href)
+                    else:
+                        self.external_links.append(href)
+
+                    self.parent_url_dict[href] = [url]
+                    self.parent_url_dict[href].append(lk.get_text())
+
+    def crawl(self, max_depth=2):
+        url_queue = deque([(self.base_url + "/", 0)])  # (url, depth)
+        all_urls = [url_queue[0][0]] # list of all urls visited
+        while len(url_queue) > 0:
+            cur_url, depth = url_queue.popleft()
+            if depth > max_depth:
+                break
+
+            self._extract_links(cur_url)
+
+            for link in self.internal_links:
+                if link not in all_urls:
+                    url_queue.append((link, depth + 1))
+                    all_urls.append(link)
