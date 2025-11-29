ca3427c59eda7badf5c84e33addd7ae8768c9844
Akshat Jaimini <destrex271@gmail.com>
2025-11-29 21:52:59 +0530

    Add libyaml-dev and build-essential to setup script

--- src/workflow_utils/setup.sh
+++ src/workflow_utils/setup.sh
@@ -17,6 +17,7 @@ database="DATABASES = {\n\t'default': {\n\t\t'ENGINE': 'django.db.backends.postg
 # Build System dependencies
 sudo apt update && sudo apt install git -y 
 # sudo apt-get install -y postgresql-client python3-dev python3-pip firefox libnss3 libtidy-dev
+sudo apt-get install -y libyaml-dev build-essential
 sudo apt-get install -y \
     postgresql-client \
     firefox \
