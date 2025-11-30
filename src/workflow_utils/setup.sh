0fb5e9217ec2912c12137ee892dcb43693394b74
Akshat Jaimini <destrex271@gmail.com>
2025-11-30 15:24:56 +0530

    fix: use python instead of xvfb

--- src/workflow_utils/setup.sh
+++ src/workflow_utils/setup.sh
@@ -131,7 +131,7 @@ PGPASSWORD=postgres psql -h postgres -U postgres -a -f sql/varnish_local.sql
 # yes | ./pgweb/load_initial_data.sh
 # ./manage.py test --pattern="tests_*.py" --keepdb --verbosity=2 2>&1 | tee -a ../../final_report.log
 # ./manage.py test --pattern="tests_re*.py" --keepdb --verbosity=2 2>&1 | tee -a ../../final_report.log
-xvfb-run python3 manage.py test --pattern="tests_*.py" --keepdb --verbosity=2 2>&1 | tee -a ../../final_report.log
+python3 manage.py test --pattern="tests_*.py" --keepdb --verbosity=2 2>&1 | tee -a ../../final_report.log
 
 python3 ../../utils/process_logs.py
 cat ../../final_report.log
